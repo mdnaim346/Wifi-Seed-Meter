7b260ac15f479b6a66d78d5853d6afd48f2cc846
mdnaim346 <mdnaimreza66@gmail.com>
2026-04-16 08:36:14 +0600

    INitial repo created

---
+++ __manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'Speed Meter',
+    'version' : '1.2',
+    'sequence': 10,
+    'summary': 'Simple Speed Meter',
+    'description': """
+    """,
+    'depends': ['sale'],
+    'data': [
+        'security/ir.model.access.csv',
+        
+        
+        
+    ],
+    
+    'installable': True,
+    'application': True,
+   
+    'assets': {
+        'web.assets_backend': [],
+            
+        'web.assets_frontend': [
+        ],
+    },
+    
+}
